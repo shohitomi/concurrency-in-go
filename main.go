eb767c8532c29242b6558c61affe19053b5a0ec3
sho <shohitomi25@gmail.com>
2023-09-09 15:05:24 +0900

    deadlock example

--- main.go
+++ main.go
@@ -3,22 +3,31 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
-func main() {
-	var memoryAccess sync.Mutex
-	var value int
-	go func() {
-		memoryAccess.Lock()
-		value++
-		memoryAccess.Unlock()
-	}()
+type value struct {
+	mu    sync.Mutex
+	value int
+}
 
-	memoryAccess.Lock()
-	if value == 0 {
-		fmt.Printf("the value is 0.\n")
-	} else {
-		fmt.Printf("the value is %v.\n", value)
+func main() {
+	var wg sync.WaitGroup
+	printSum := func(v1, v2 *value) {
+		defer wg.Done()
+		v1.mu.Lock()
+		defer v1.mu.Unlock()
+
+		time.Sleep(2 * time.Second)
+		v2.mu.Lock()
+		defer v2.mu.Unlock()
+
+		fmt.Printf("sum=%v\n", v1.value+v2.value)
 	}
-	memoryAccess.Unlock()
+
+	var a, b value
+	wg.Add(2)
+	go printSum(&a, &b)
+	go printSum(&b, &a)
+	wg.Wait()
 }
