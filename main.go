33deff614834d7e006b805a90e3eaf1a26416a6a
sho <shohitomi25@gmail.com>
2023-09-09 16:53:25 +0900

    updated range example

--- main.go
+++ main.go
@@ -9,10 +9,10 @@ func main() {
 	var wg sync.WaitGroup
 	for _, salutation := range []string{"hello", "greetings", "good day"} {
 		wg.Add(1)
-		go func() {
+		go func(salutation string) {
 			defer wg.Done()
 			fmt.Println(salutation)
-		}()
+		}(salutation)
 	}
 	wg.Wait()
 }
