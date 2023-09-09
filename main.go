1398c36e458206fd050e2add023481caa36df50c
sho <shohitomi25@gmail.com>
2023-09-09 21:12:10 +0900

    Once example

--- main.go
+++ main.go
@@ -6,40 +6,23 @@ import (
 )
 
 func main() {
-	type Button struct { // ❶
-		Clicked *sync.Cond
-	}
-	button := Button{Clicked: sync.NewCond(&sync.Mutex{})}
+	var count int
 
-	subscribe := func(c *sync.Cond, fn func()) { // ❷
-		var goroutineRunning sync.WaitGroup
-		goroutineRunning.Add(1)
+	increment := func() {
+		count++
+	}
+
+	var once sync.Once
+
+	var increments sync.WaitGroup
+	increments.Add(100)
+	for i := 0; i < 100; i++ {
 		go func() {
-			goroutineRunning.Done()
-			c.L.Lock()
-			defer c.L.Unlock()
-			c.Wait()
-			fn()
+			defer increments.Done()
+			once.Do(increment)
 		}()
-		goroutineRunning.Wait()
 	}
 
-	var clickRegistered sync.WaitGroup // ❸
-	clickRegistered.Add(3)
-	subscribe(button.Clicked, func() { // ❹
-		fmt.Println("Maximizing window.")
-		clickRegistered.Done()
-	})
-	subscribe(button.Clicked, func() { // ❺
-		fmt.Println("Displaying annoying dialog box!")
-		clickRegistered.Done()
-	})
-	subscribe(button.Clicked, func() { // ❻
-		fmt.Println("Mouse clicked.")
-		clickRegistered.Done()
-	})
-
-	button.Clicked.Broadcast() // ❼
-
-	clickRegistered.Wait()
+	increments.Wait()
+	fmt.Printf("Count is %d\n", count)
 }
