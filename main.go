aa6cbc8bec048aa10a9cd2add562175e7d26f971
sho <shohitomi25@gmail.com>
2023-09-09 22:06:36 +0900

    select example2

--- main.go
+++ main.go
@@ -2,20 +2,23 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-	start := time.Now()
-	c := make(chan interface{})
-	go func() {
-		time.Sleep(5 * time.Second)
-		close(c) // ❶
-	}()
+	c1 := make(chan interface{})
+	close(c1)
+	c2 := make(chan interface{})
+	close(c2)
 
-	fmt.Println("Blocking on read...")
-	select {
-	case <-c: // ❷
-		fmt.Printf("Unblocked %v later.\n", time.Since(start))
+	var c1Count, c2Count int
+	for i := 1000; i >= 0; i-- {
+		select {
+		case <-c1:
+			c1Count++
+		case <-c2:
+			c2Count++
+		}
 	}
+
+	fmt.Printf("c1Count: %d\nc2Count: %d\n", c1Count, c2Count)
 }
