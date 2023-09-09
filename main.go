9379abdb3a522b3b87246b6d423541650c3df4b4
sho <shohitomi25@gmail.com>
2023-09-09 17:06:18 +0900

    sync example2

--- main.go
+++ main.go
@@ -3,26 +3,19 @@ package main
 import (
 	"fmt"
 	"sync"
-	"time"
 )
 
 func main() {
+	hello := func(wg *sync.WaitGroup, id int) {
+		defer wg.Done()
+		fmt.Printf("Hello from %v!\n", id)
+	}
+
+	const numGreeters = 5
 	var wg sync.WaitGroup
-
-	wg.Add(1) // ❶
-	go func() {
-		defer wg.Done() // ❷
-		fmt.Println("1st goroutine sleeping...")
-		time.Sleep(1)
-	}()
-
-	wg.Add(1) // ❶
-	go func() {
-		defer wg.Done() // ❷
-		fmt.Println("2nd goroutine sleeping...")
-		time.Sleep(2)
-	}()
-
-	wg.Wait() // ❸
-	fmt.Println("All goroutines complete.")
+	wg.Add(numGreeters)
+	for i := 0; i < numGreeters; i++ {
+		go hello(&wg, i+1)
+	}
+	wg.Wait()
 }
