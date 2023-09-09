8b38029922c51421afc30c3d68def501d74cc017
sho <shohitomi25@gmail.com>
2023-09-09 17:03:51 +0900

    sync example

--- main.go
+++ main.go
@@ -2,29 +2,27 @@ package main
 
 import (
 	"fmt"
-	"runtime"
 	"sync"
+	"time"
 )
 
 func main() {
-	memConsumed := func() uint64 {
-		runtime.GC()
-		var s runtime.MemStats
-		runtime.ReadMemStats(&s)
-		return s.Sys
-	}
-
-	var c <-chan interface{}
 	var wg sync.WaitGroup
-	noop := func() { wg.Done(); <-c } // ❶
 
-	const numGoroutines = 1e4 // ❷
-	wg.Add(numGoroutines)
-	before := memConsumed() // ❸
-	for i := numGoroutines; i > 0; i-- {
-		go noop()
-	}
-	wg.Wait()
-	after := memConsumed() // ❹
-	fmt.Printf("%.3fkb", float64(after-before)/numGoroutines/1000)
+	wg.Add(1) // ❶
+	go func() {
+		defer wg.Done() // ❷
+		fmt.Println("1st goroutine sleeping...")
+		time.Sleep(1)
+	}()
+
+	wg.Add(1) // ❶
+	go func() {
+		defer wg.Done() // ❷
+		fmt.Println("2nd goroutine sleeping...")
+		time.Sleep(2)
+	}()
+
+	wg.Wait() // ❸
+	fmt.Println("All goroutines complete.")
 }
