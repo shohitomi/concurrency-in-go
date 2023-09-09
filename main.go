30e17bbce41f5be331faf2f64fc93b4e9608ada7
sho <shohitomi25@gmail.com>
2023-09-09 15:37:16 +0900

    livelock example

--- main.go
+++ main.go
@@ -1,33 +1,60 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
-type value struct {
-	mu    sync.Mutex
-	value int
-}
-
 func main() {
-	var wg sync.WaitGroup
-	printSum := func(v1, v2 *value) {
-		defer wg.Done()
-		v1.mu.Lock()
-		defer v1.mu.Unlock()
+	cadence := sync.NewCond(&sync.Mutex{})
+	go func() {
+		for range time.Tick(1 * time.Millisecond) {
+			cadence.Broadcast()
+		}
+	}()
 
-		time.Sleep(2 * time.Second)
-		v2.mu.Lock()
-		defer v2.mu.Unlock()
-
-		fmt.Printf("sum=%v\n", v1.value+v2.value)
+	takeStep := func() {
+		cadence.L.Lock()
+		cadence.Wait()
+		cadence.L.Unlock()
 	}
 
-	var a, b value
-	wg.Add(2)
-	go printSum(&a, &b)
-	go printSum(&b, &a)
-	wg.Wait()
+	tryDir := func(dirName string, dir *int32, out *bytes.Buffer) bool { // ❶
+		fmt.Fprintf(out, " %v", dirName)
+		atomic.AddInt32(dir, 1) // ❷
+		takeStep()              // ❸
+		if atomic.LoadInt32(dir) == 1 {
+			fmt.Fprint(out, ". Success!")
+			return true
+		}
+		takeStep()
+		atomic.AddInt32(dir, -1) // ❹
+		return false
+	}
+
+	var left, right int32
+	tryLeft := func(out *bytes.Buffer) bool { return tryDir("left", &left, out) }
+	tryRight := func(out *bytes.Buffer) bool { return tryDir("right", &right, out) }
+
+	walk := func(walking *sync.WaitGroup, name string) {
+		var out bytes.Buffer
+		defer func() { fmt.Println(out.String()) }()
+		defer walking.Done()
+		fmt.Fprintf(&out, "%v is trying to scoot:", name)
+		for i := 0; i < 5; i++ { // ❶
+			if tryLeft(&out) || tryRight(&out) { // ❷
+				return
+			}
+		}
+		fmt.Fprintf(&out, "\n%v tosses her hands up in exasperation!", name)
+	}
+
+	var peopleInHallway sync.WaitGroup // ❸
+	peopleInHallway.Add(2)
+	go walk(&peopleInHallway, "Alice")
+	go walk(&peopleInHallway, "Barbara")
+	peopleInHallway.Wait()
 }
