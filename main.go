7feb3b79ddc58c67e46773531cff5bb6dbe72216
sho <shohitomi25@gmail.com>
2023-09-09 14:52:28 +0900

    bad example

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	var data int
 	go func() {
 		data++
 	}()
+	time.Sleep(1 * time.Second)
 	if data == 0 {
 		fmt.Printf("the value is %v.\n", data)
 	}
